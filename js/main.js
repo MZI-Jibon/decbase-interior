949b73e1dc5e3cd18a26f700a40f44885b3e1f4e
UiHut-Jibon <113233436+UiHut-Jibon@users.noreply.github.com>
2023-01-29 08:39:43 +0600

    third commit

--- js/main.js
+++ js/main.js
@@ -28,4 +28,22 @@ $(document).ready(function () {
             }
         }
     })
+
+    $('.all-testimonials').owlCarousel({
+        loop: true,
+        margin: 20,
+        nav: true,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            600: {
+                items: 1,
+            },
+            1000: {
+                items: 2,
+            }
+        }
+    })
 });
